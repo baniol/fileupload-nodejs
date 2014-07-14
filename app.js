60e7ee20c23e8e7ff0cb1baadd9aed04ad18af3e
root <root@local-centos.(none)>
2014-07-14 09:07:20 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+//var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var upload = require('jquery-file-upload-middleware');
+
+process.on('uncaughtException', function(err) {
+    console.log('Threw Exception: ', err);
+});
+
+var dirs = require('./config').directors;
+
+var app = express();
+
+app.use('/upload/location', upload.fileHandler({
+    tmpDir: __dirname + dirs.temp,
+    uploadDir: __dirname + dirs.location,
+    uploadUrl: dirs.location_url,
+    acceptFileTypes: /\.(gif|jpe?g|png)$/i,
+    maxFileSize: 10000000, // 10M
+    imageVersions: {
+        thumbnail: {
+            width: 80,
+            height: 80
+        }
+    }
+    //imageTypes: /\.(gif|jpe?g|png)$/i
+}));
+
+app.use('/upload/location/list', function (req, res) {
+    upload.fileManager({
+        uploadDir: function () {
+            return __dirname + dirs.location;
+        },
+        uploadUrl: function () {
+            return dirs.locationUrl;
+        }
+    }).getFiles(function (files) {
+        res.json(files);
+    });
+});
+
+// Actions on upload events
+upload.on('end', function (fileInfo) {
+    console.log(fileInfo);
+});
+
+upload.on('delete', function (fileName) {
+    console.log(fileName);
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+//app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('upload', { title: 'Blueimp fileuploader' });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
